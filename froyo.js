426420aa2c8d5cdedc235e038c58f5b75437b6cc
Sydney Mitchell <Sydney@Mac.attlocal.net>
2024-12-17 13:44:46 -0600

    updated order of code

--- froyo.js
+++ froyo.js
@@ -1,17 +1,6 @@
 //INPUT: V,V,V,S,C,C
 //OUTPUT: {V: 3, S: 1, C: 2}
 
-//prompt user to enter froyo flavors separated by commas
-const userInputString = prompt("Enter your froyo flavors below:", "vanilla,vanilla,vanilla,strawberry,coffee,coffee");
-
-//split string into array of strings & call function
-if (userInputString) {
-  const flavorsArray = userInputString.split(",");
-  const flavorCounts = countFrequencies(flavorsArray);
-  console.log("Flavor order summary: ");
-  console.table(flavorCounts); 
-}
-
 //call function to count occurrence of each flavor
 function countFrequencies(flavorsArray) {
   const flavorCounts = {};
@@ -20,4 +9,14 @@ function countFrequencies(flavorsArray) {
     flavorCounts[flavor] = (flavorCounts[flavor] || 0) + 1;
   }  
   return flavorCounts;
+}
+
+//prompt user to enter froyo flavors separated by commas
+const userInputString = prompt("Enter your froyo flavors below:", "vanilla,vanilla,vanilla,strawberry,coffee,coffee");
+//split string into array of strings & call function
+if (userInputString) {
+  const flavorsArray = userInputString.split(",");
+  const flavorCounts = countFrequencies(flavorsArray);
+  console.log("Flavor order summary: ");
+  console.table(flavorCounts); 
 }
