871eb40ace5ea4e49235751f2e63f697b5e20189
Sydney Mitchell <Sydney@Mac.attlocal.net>
2024-12-17 13:38:50 -0600

    completed workshop

---
+++ froyo.js
@@ -0,0 +1,23 @@
+//INPUT: V,V,V,S,C,C
+//OUTPUT: {V: 3, S: 1, C: 2}
+
+//prompt user to enter froyo flavors separated by commas
+const userInputString = prompt("Enter your froyo flavors below:", "vanilla,vanilla,vanilla,strawberry,coffee,coffee");
+
+//split string into array of strings & call function
+if (userInputString) {
+  const flavorsArray = userInputString.split(",");
+  const flavorCounts = countFrequencies(flavorsArray);
+  console.log("Flavor order summary: ");
+  console.table(flavorCounts); 
+}
+
+//call function to count occurrence of each flavor
+function countFrequencies(flavorsArray) {
+  const flavorCounts = {};
+//loop thru array to count each flavor
+  for (let flavor of flavorsArray) {
+    flavorCounts[flavor] = (flavorCounts[flavor] || 0) + 1;
+  }  
+  return flavorCounts;
+}
